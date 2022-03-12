efad177991f96ff6478fb717095c9294ddc055d1
Jan Ulik <ulik.jan@gmail.com>
2022-03-12 16:30:45 +0100

    no coment

--- sortingGraph.js
+++ sortingGraph.js
@@ -1,12 +1,11 @@
-////master branch working everithing fine////
 const canvasA = document.getElementById('sortingGraphC');
 const ctxA = canvasA.getContext('2d');
-var valueOfBigLoop = 0;
+const ARRAY_LENGTH_a = 300;
 
-/*generate values***********************/
-const ARRAY_LENGTH_a = 300;                
-const randomArray_a = [];
 
+/*variable type changed from cost to var in order to have possibility to asign empty array befor loading new values*/               
+var randomArray_a = [];
+/*function sleep used to slow down the motion of sorting in order to have nice visualization. */
 function sleep(milliseconds) {
    const date = Date.now();
    let currentDate = null;
@@ -15,36 +14,53 @@ function sleep(milliseconds) {
    } while (currentDate - date < milliseconds);
  }
 
-makeValues = function(){
+/*function cleanup used in drawSortedValues() in order to cleanup canvas before each iteration and in drawValues() in order to cleanup before new values load to canvas" */
+function cleanup(){
+   ctxA.clearRect(0, 0, canvasA.width, canvasA.height);
+   }
+/*function to generate values called by drawValues()*/
+function makeValues(){
+   randomArray_a = []
    for(let i = 0; i<ARRAY_LENGTH_a; i++){
        randomArray_a.push(Math.floor(Math.random()*400))
        }
 }
-makeValues();
 
-/*draw values into the canvas*****************/
-
-   var s = 1;
+/*draw values into the canvas called by onclick ebvent button load*****************/
+function drawValues(){
+   cleanup()
+   makeValues()
+   var s = 10;
    for(var i = 0; i <= randomArray_a.length; i++){   
-   s += 2; 
+   s += 3; 
    ctxA.fillRect(s, canvasA.height - randomArray_a[i], 1, randomArray_a[i])
    }
+}
 
-/*sort values******************************/
+
+/*this function is redrawing values after each run of sortValuesA() and is called by BigSort()*/ 
+function drawSortedValues(){
+   cleanup();
+   //here will be other function that will overvrite the canvas with new positions two bars currently changing the position//
+   var s = 10;
+   for(var i = 0; i <= randomArray_a.length; i++){    
+   ctxA.fillRect(s, canvasA.height - randomArray_a[i], 1, randomArray_a[i])
+   s += 3;
+   }
+}
+//IMPORTANT COMMENT//
+/*the sortValuesA(m) is the function which is called after each evaluation of array sequence. Evaluation and calling happens in bigSort().  The atribute m provides the value(incremented in each call) for outer loop which always does only one iteration and ends. This is necesary because the browser doesnt want to refresh while any loop is running*/
 function sortValuesA(m){
-
    function swap(arr,xp, yp){       
        var temp = arr[xp];
        arr[xp] = arr[yp];
        arr[yp] = temp;
-      }    
+      }
+   //this is selectionSort copied from web, with updated outer loop//       
    function selectionSort(arr,  n){
+         var i, j, min_idx;  
 
-         var i, j, min_idx;       
-         // One by one move boundary of unsorted subarray
-         //change to 1 does one iteration but.........../
-         //problem je v tom ze loop najde najnizsiu hodnotu a da ju na prve miesto potom vykreslim graf ale ked je funkcia zavolana znova tak loop zacina znova na prvom mieste a nizsiu hodnotu nenajde, naproti tomu funkcia check for sequence vzdy najde nezrovnalosti a preto vznikne nekonecny program.  
-         
+         // HERE THE VALUE IS ALWAYS BIGGER ONLY BY 1 THEN I//
             for (i = m-1; i < m; i++)                       
             {
                // Find the minimum element in unsorted array        
@@ -59,27 +75,27 @@ function sortValuesA(m){
       }
    var nl = randomArray_a.length;
    selectionSort(randomArray_a, nl);
-   }
-//sortValues();
-//checkForSequence();   
-
+}
+/*the variable valueOfBigLoop is used also in onclick function for sorting because the bigSort() needs it to 0 before it starts, this is the value/atribute being incremented for outer loop*/
+var valueOfBigLoop = 0;
 function bigSort(){
-   var unsorted = false;
-   checkForSequence();
+   var sorted = false;
+   //checkForSequence function checks if the array is sorted or not yet
+   checkForSequence();   
    function checkForSequence(){
       var arrLenght = randomArray_a.length;
       for(var i = 0; i<=arrLenght; i++){
          if(randomArray_a[i]>randomArray_a[i+1]){
-            unsorted = true;
+            sorted = false;
             break;
             }
          else
-            unsorted = false;
+            sorted = true;
             continue;   
       }
    }  
-
-   if(unsorted == true){
+   //condition which decides if the sorting has to continue or not in case the array is sorted 
+   if(sorted == false){
       sleep(30)
       valueOfBigLoop += 1;
       sortValuesA(valueOfBigLoop);
@@ -90,33 +106,10 @@ function bigSort(){
       drawSortedValues()
 }
 
-bigSort();
-  
-/*draw sorted values*/
-function drawSortedValues(){
-   function cleanup(){
-      ctxA.clearRect(0, 0, canvasA.width, canvasA.height);
-      }
-   cleanup();
+/*buttons*/
+var showValuesButton = document.getElementById("load")
+showValuesButton.onclick = function(){drawValues()}
 
-   ctxA.fillRect(20,20, 50, 50);
-
-   var s = 1;
-   for(var i = 0; i <= randomArray_a.length; i++){   
-   s += 3; 
-   ctxA.fillRect(s, canvasA.height - randomArray_a[i], 1, randomArray_a[i])
-   }
-}
-
-
-/*ctx.stroke;
-
-showValues = function(){
-
-   for(let i = 0; i<ARRAY_LENGTH; i++){
-      randomArray.push(Math.random())
-      }
-  
-
-}*/
+var sortValuesButton = document.getElementById("sort")
+sortValuesButton.onclick = function(){valueOfBigLoop = 0;bigSort()}
 
